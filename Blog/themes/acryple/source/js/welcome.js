a6a4a5a9ccb5127d14c837676a7207a518d112f5
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-11-04 09:27:44 +0800

    style: prettier

--- Blog/themes/acryple/source/js/welcome.js
+++ Blog/themes/acryple/source/js/welcome.js
@@ -1,38 +1,92 @@
-if ('serviceWorker' in window.navigator && navigator.serviceWorker.controller) {
-    // 发送更新指令到sw
-    // 'update'改为'refresh'即可触发缓存刷新
-    navigator.serviceWorker.controller.postMessage('update')
-    navigator.serviceWorker.addEventListener('message', event => {
-        const data = event.data
-        switch (data.type) {
-            case 'update':
-                // 这里写更新后的操作
-                break
-            case 'refresh':
-                // 这里写强刷缓存后的操作
-                break
-            default:
-                console.error(`未知事件：${data.type}`)
-        }
-    })
-}
+// Service Worker 处理
+const handleServiceWorker = () => {
+    if (!('serviceWorker' in navigator) || !navigator.serviceWorker.controller) return;
 
-if (localStorage.getItem("popWelcomeWindow") != "0") {
-    if(document.referrer==undefined||document.referrer.indexOf("yumetsuki.moe")!=-1||document.referrer.indexOf("ariasaka.top")!=-1){
-        Snackbar.show({
-            pos: "top-right",
-            showAction: false,
-            text: '欢迎访问本站！'
-        })
-    }else{
-        Snackbar.show({
-                pos: "top-right",
-                showAction: false,
-                text: `欢迎来自${document.referrer.split("://")[1].split("/")[0]}的童鞋访问本站！`
-            })
+    navigator.serviceWorker.controller.postMessage('update');
+    navigator.serviceWorker.addEventListener('message', ({ data }) => {
+        const handlers = {
+            update: () => {
+                // 更新后的操作
+            },
+            refresh: () => {
+                // 强刷缓存后的操作
+            }
+        };
+
+        handlers[data.type]?.() || console.error(`未知事件：${data.type}`);
+    });
+};
+
+// 欢迎提示处理
+const showWelcomeMessage = () => {
+    if (localStorage.getItem("popWelcomeWindow") === "0") return;
+
+    const trustedDomains = ['yumetsuki.moe', 'ariasaka.top'];
+    const referrerDomain = document.referrer ? new URL(document.referrer).hostname : '';
+
+    const message = trustedDomains.some(domain => referrerDomain.includes(domain)) || !referrerDomain
+        ? '欢迎访问本站！'
+        : `欢迎来自${referrerDomain}的童鞋访问本站！`;
+
+    Snackbar.show({
+        pos: "top-right",
+        showAction: false,
+        text: message
+    });
+
+    if (!trustedDomains.some(domain => referrerDomain.includes(domain))) {
         localStorage.setItem("popWelcomeWindow", "0");
     }
-}
+};
+
+// 浏览器版本检测优化
+const BROWSER_VERSION_LIMITS = {
+    Edge: 90,
+    Firefox: 90,
+    OPR: 80,
+    Chrome: 90,
+    Safari: null // Safari 暂不处理
+};
+
+const getBrowserInfo = () => {
+    const ua = navigator.userAgent;
+    for (const [browser, version] of Object.entries(BROWSER_VERSION_LIMITS)) {
+        if (ua.includes(browser)) {
+            const match = ua.match(new RegExp(`${browser}\\/([\\d]+)`));
+            return { browser, version: match ? parseInt(match[1]) : 0 };
+        }
+    }
+    return null;
+};
+
+const browserVersion = () => {
+    const browserInfo = getBrowserInfo();
+    if (!browserInfo) return;
+
+    const minVersion = BROWSER_VERSION_LIMITS[browserInfo.browser];
+    if (minVersion && browserInfo.version < minVersion) {
+        Snackbar.show({
+            text: '浏览器版本较低，网站样式可能错乱',
+            actionText: '关闭',
+            duration: '6000',
+            pos: 'bottom-right'
+        });
+    }
+};
+
+// Cookie 相关函数优化
+const setCookies = (obj, limitTime) => {
+    const expires = new Date(Date.now() + limitTime * 24 * 60 * 60 * 1000).toGMTString();
+    Object.entries(obj).forEach(([key, value]) => {
+        document.cookie = `${key}=${value};expires=${expires}`;
+    });
+};
+
+const getCookie = name => {
+    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]*)(;|$)`));
+    return match ? decodeURIComponent(match[2]) : null;
+};
+
 if (sessionStorage.getItem("popCookieWindow") != "0") {
     setTimeout(function () {
         Snackbar.show({
@@ -47,59 +101,7 @@ if (sessionStorage.getItem("popCookieWindow") != "0") {
 }
 sessionStorage.setItem("popCookieWindow", "0");
 
-function browserTC() {
-    btf.snackbarShow("");
-    Snackbar.show({
-        text: '浏览器版本较低，网站样式可能错乱',
-        actionText: '关闭',
-        duration: '6000',
-        pos: 'bottom-right'
-    });
-}
-function browserVersion() {
-    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
-    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
-    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
-    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //Edge浏览器
-    var isFirefox = userAgent.indexOf("Firefox") > -1; //Firefox浏览器
-    var isOpera = userAgent.indexOf("Opera")>-1 || userAgent.indexOf("OPR")>-1 ; //Opera浏览器
-    var isChrome = userAgent.indexOf("Chrome")>-1 && userAgent.indexOf("Safari")>-1 && userAgent.indexOf("Edge")==-1 && userAgent.indexOf("OPR")==-1; //Chrome浏览器
-    var isSafari = userAgent.indexOf("Safari")>-1 && userAgent.indexOf("Chrome")==-1 && userAgent.indexOf("Edge")==-1 && userAgent.indexOf("OPR")==-1; //Safari浏览器
-    if(isEdge) {
-        if(userAgent.split('Edge/')[1].split('.')[0]<90){
-            browserTC()
-        }
-    } else if(isFirefox) {
-        if(userAgent.split('Firefox/')[1].split('.')[0]<90){
-            browserTC()
-        }
-    } else if(isOpera) {
-        if(userAgent.split('OPR/')[1].split('.')[0]<80){
-            browserTC()
-        }
-    } else if(isChrome) {
-        if(userAgent.split('Chrome/')[1].split('.')[0]<90){
-            browserTC()
-        }
-    } else if(isSafari) {
-        //不知道Safari哪个版本是该淘汰的老旧版本
-    }
-}
-//2022-10-29修正了一个错误：过期时间应使用toGMTString()，而不是toUTCString()，否则实际过期时间在中国差了8小时
-function setCookies(obj, limitTime) {
-	let data = new Date(new Date().getTime() + limitTime * 24 * 60 * 60 * 1000).toGMTString()
-	for (let i in obj) {
-		document.cookie = i + '=' + obj[i] + ';expires=' + data
-	}
-}
-function getCookie(name) {
-	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-	if (arr = document.cookie.match(reg))
-		return unescape(arr[2]);
-	else
-		return null;
-}
-if(getCookie('browsertc')!=1){
+if (getCookie('browsertc') != 1) {
     setCookies({
         browsertc: 1,
     }, 1);
